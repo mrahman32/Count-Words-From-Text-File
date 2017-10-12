469fb571754f5282e2a2151aaf1c6eabffde72dc
mrahman32 <mrahman.cse32@gmail.com>
2017-10-12 11:44:59 +0600

    Initial

---
+++ CountWordsFromTextFile/CountWordsFromTextFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace CountWordsFromTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = 0;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            using (var sr = new StreamReader("D:/wap.txt"))
+            {
+                
+                while (!sr.EndOfStream)
+                {
+                    var counts = sr
+                        .ReadLine()
+                        .Split(' ')
+                        .GroupBy(s => s)
+                        .Select(g => new { Word = g.Key, Count = g.Count() });
+                    var wc = counts.Sum(i => i.Count);
+                    total += (wc == null) ? 0 : wc;
+                }
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine(total + "\n Time Required : " + elapsedTime);
+            Console.ReadKey();
+        }
+    }
+}
